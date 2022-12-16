c5fc6dff5c4c6a3c37b50a596088395a4e8da401
GabrielPedraga <107312107+GabrielPedraga@users.noreply.github.com>
2022-12-16 07:04:10 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import { useState, useEffect, useRef} from 'react';
+import './App.css';
+import { motion } from 'framer-motion'
+
+import image1 from '../src/img/img1.jpg'
+import image2 from '../src/img/img2.jpg'
+import image3 from '../src/img/img3.jpg'
+import image4 from '../src/img/img4.jpg'
+
+const images = [image1, image2, image3, image4]
+
+function App() {
+const carousel = useRef();
+const [width, setWidth] = useState(0)
+
+useEffect(() => {
+   console.log(carousel.current?.scrollWidth,carousel.current?.offseWidth)
+   setWidth(carousel.current?.scrollWidth - carousel.current?.offseWidth)
+},[])
+
+  return (
+    <div className="App">
+    
+    <motion.div ref={carousel} className="carousel" whileTap={{ cursor: "grabbing" }}>
+      <motion.div className="inner"
+      drag="x"
+      dragConstraints={{ right: 0,left: -width}}
+      initial={{ x: 100}}
+      animate={{ x: 0}}
+      transition={{ duration: 0.8}}
+      >
+        
+        {images.map(image =>(
+          <motion.div className="item" key={image}>
+            <img src={image} alt="Texto alt"/>
+          </motion.div>
+        ))}
+
+      </motion.div>
+    </motion.div>
+
+    </div>
+  );
+}
+
+export default App;
